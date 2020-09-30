eff93d2fcbe8fecd995ae6dc05ea6f28ff7a68cf
git-austinmccollom <austinmccollom@gmail.com>
2020-09-29 22:59:31 -0500

    post comment complete

--- postRoutes.js
+++ postRoutes.js
@@ -19,7 +19,7 @@ router.post("/", (req, res) => {
             res.status(201).json(findRes);
           })
           .catch((findErr) => {
-            res.json({ errorMessage: "server error adding to the database" });
+            res.status(500).json({ errorMessage: "server error adding to the database" });
           });
 
         // res.status(201).json({ success: "yes?" });
@@ -32,6 +32,39 @@ router.post("/", (req, res) => {
       });
   }
 });
+
+router.post("/:id/comments", (req, res) => {
+    const comment = {
+        text: req.body.text,
+        post_id: req.params.id
+    }
+    if ( comment.text ) {
+        data.findById(comment.id)
+        .then( findRes => {
+            if (findRes === []) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                data.insertComment(comment)
+                .then( commentRes => {
+                    data.findCommentById( commentRes.id )
+                    .then( findCommentRes => {
+                        res.status(201).json(findCommentRes)
+                    })
+                    .catch( findCommentErr => {
+                        res.status(500).json({ errorMessage: "could not find after creating"})
+                    })
+                })
+            }
+        })
+        .catch( findErr => {
+            res.status(500).json({ message: "could not check database" })
+        })
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide text for the comment.",
+          })
+    }
+} )
 
 // router.get('/', (req, res) => {
 //     res.status(200).send({})
