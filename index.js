f22efd82d47bf0e251b9afc866f7a31d0028c70b
git-austinmccollom <austinmccollom@gmail.com>
2020-09-29 21:05:42 -0500

    set up route for posts and server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const postsRoutes = require('./postRoutes.js');
+
+const server = express();
+
+server.use('/api/posts', postsRoutes );
+
+server.use('/', (req, res) => res.send('API up and running!'));
+server.listen(8000, () => console.log('API running on port 8000'));
+
