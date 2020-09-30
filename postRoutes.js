d5d04d42cfbd003ec2153bbb2e49fae55ba42898
git-austinmccollom <austinmccollom@gmail.com>
2020-09-30 00:32:37 -0500

    fixed delete in case of wrong id. Worked on put

--- postRoutes.js
+++ postRoutes.js
@@ -45,11 +45,9 @@ router.post("/:id/comments", (req, res) => {
       .findById(comment.id)
       .then((findRes) => {
         if (findRes.length < 1) {
-          res
-            .status(404)
-            .json({
-              message: "The post with the specified ID does not exist.",
-            });
+          res.status(404).json({
+            message: "The post with the specified ID does not exist.",
+          });
         } else {
           data.insertComment(comment).then((commentRes) => {
             data
@@ -75,67 +73,112 @@ router.post("/:id/comments", (req, res) => {
   }
 });
 
-router.get('/', (req, res) => {
-    data.find()
-    .then( findRes => {
-        res.status(200).json(findRes)
+router.get("/", (req, res) => {
+  data
+    .find()
+    .then((findRes) => {
+      res.status(200).json(findRes);
     })
-    .then( findErr => {
-        res.status(500).json({ error: "The posts information could not be retrieved." })
+    .then((findErr) => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  data
+    .findById(id)
+    .then((findRes) => {
+      if (findRes.length < 1) {
+        res
+          .status(404)
+          .json({ error: "The posts information could not be retrieved." });
+      } else {
+        res.status(200).json(findRes);
+      }
     })
-})
+    .then((findErr) => {
+      // res.status(500).json({ error: "unknown server error" });
+      res.status(500).json(findErr);
+    });
+});
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
+router.get("/:id/comments", (req, res) => {
+  const id = req.params.id;
 
-    data.findById(id)
-    .then( findRes => {
-        if (findRes.length < 1) {
-            res.status(404).json({ error: "The posts information could not be retrieved." })
+  data
+    .findPostComments(id)
+    .then((findRes) => {
+      if (findRes.length < 1) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json(findRes);
+      }
+    })
+    .then((findErr) => {
+      // res.status(500).json({ error: "unknown server error" });
+      res.status(500).json(findErr);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  data
+    .remove(id)
+    .then((findRes) => {
+      if (findRes !== 1) {
+        res
+          .status(404)
+          //   .json(findRes);
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json({ message: "post deleted" });
+      }
+    })
+    .then((findErr) => {
+      // res.status(500).json({ error: "unknown server error" });
+      res.status(500).json(findErr);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  } else {
+    data
+      .update(req.params.id, req.body)
+      .then((dbRes) => {
+        if (dbRes.length() < 1) {
+          res.status(404).json({
+            message: "error?",
+          });
         } else {
-            res.status(200).json(findRes)
+          data
+            .findById(req.params.id)
+            .then((findRes) => {
+              res.status(200).json(findRes);
+            })
+            .catch((findErr) => {
+              res
+                .status(500)
+                .json({ errorMessage: "could not find post after updating" });
+            });
         }
-    })
-    .then( findErr => {
-        // res.status(500).json({ error: "unknown server error" });
-        res.status(500).json(findErr);
-    })
-})
-
-router.get('/:id/comments', (req, res) => {
-    const id = req.params.id;
-
-    data.findPostComments( id )
-    .then( findRes => {
-        if (findRes.length < 1) {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        } else {
-            res.status(200).json(findRes)
-        }
-    })
-    .then( findErr => {
-        // res.status(500).json({ error: "unknown server error" });
-        res.status(500).json(findErr);
-    })
-})
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-
-    data.remove( id )
-    .then( findRes => {
-        if (findRes.length < 1) {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        } else {
-            res.status(200).json({ message: "post deleted" })
-        }
-    })
-    .then( findErr => {
-        // res.status(500).json({ error: "unknown server error" });
-        res.status(500).json(findErr);
-    })
-})
-
-
+      })
+      .catch((err) => {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      });
+  }
+});
 
 module.exports = router;
